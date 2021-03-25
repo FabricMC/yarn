54515a1afc53eae01e165a8f3f649a7481089322
Shnupbups <shnupbups@gmail.com>
2021-03-25 12:28:16 +1100

    World Events and Global Events Unpicked

---
+++ src/constants/java/net/fabricmc/yarn/constants/GlobalEvents.java
@@ -0,0 +1,21 @@
+package net.fabricmc.yarn.constants;
+
+public final class GlobalEvents {
+	/**
+	 * A Wither is spawned
+	 * Plays the wither spawn sound event
+	 */
+	public static final int WITHER_SPAWNS = 1023;
+
+	/**
+	 * An Ender Dragon dies
+	 * Plays the ender dragon death sound event
+	 */
+	public static final int ENDER_DRAGON_DIES = 1028;
+
+	/**
+	 * An End Portal is opened
+	 * Plays the end portal spawn sound event
+	 */
+	public static final int END_PORTAL_OPENED = 1038;
+}
