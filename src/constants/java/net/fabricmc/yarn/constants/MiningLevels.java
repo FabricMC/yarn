8791ccaf7558ecbe952dc7addeed69bca65646a6
Alexander <shnupbups@gmail.com>
2021-03-26 02:41:23 +1100

    Mining Levels unpicked
    Apparently unpick can't place its constants in enum constructor parameters which is the only place vanilla would use these, so I don't know but here

---
+++ src/constants/java/net/fabricmc/yarn/constants/MiningLevels.java
@@ -0,0 +1,34 @@
+package net.fabricmc.yarn.constants;
+
+public final class MiningLevels {
+	/**
+	 * Does not require a tool to harvest.
+	 */
+	public static final int HAND = -1;
+
+	/**
+	 * Requires a wooden tool or better to harvest.
+	 * Also used by golden tools.
+	 */
+	public static final int WOOD = 0;
+
+	/**
+	 * Requires a stone tool or better to harvest.
+	 */
+	public static final int STONE = 1;
+
+	/**
+	 * Requires an iron tool or better to harvest.
+	 */
+	public static final int IRON = 2;
+
+	/**
+	 * Requires a diamond tool or better to harvest.
+	 */
+	public static final int DIAMOND = 3;
+
+	/**
+	 * Requires a netherite tool or better to harvest.
+	 */
+	public static final int NETHERITE = 4;
+}
