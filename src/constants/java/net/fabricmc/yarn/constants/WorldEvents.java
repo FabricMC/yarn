426a9c4d6ed8ecc2ea7c9f426892b7624a91014d
Shnupbups <shnupbups@gmail.com>
2021-03-26 12:45:22 +1100

    stuff
    consolidated WorldEvents and GlobalEvents into one
    renamed CROPS_FERTILIZED to PLANT_FERTILIZED
    added packet stuff to target methods
    added punctuation to javadoc

--- src/constants/java/net/fabricmc/yarn/constants/WorldEvents.java
+++ src/constants/java/net/fabricmc/yarn/constants/WorldEvents.java
@@ -2,412 +2,433 @@ package net.fabricmc.yarn.constants;
 
 public final class WorldEvents {
 	/**
-	 * Dispenser dispenses an item
-	 * Plays the dispensing sound event
+	 * A Dispenser dispenses an item.
+	 * Plays the dispensing sound event.
 	 */
 	public static final int DISPENSER_DISPENSES = 1000;
 
 	/**
-	 * Dispenser fails to dispense an item
-	 * Plays the dispenser fail sound event
+	 * A Dispenser fails to dispense an item.
+	 * Plays the dispenser fail sound event.
 	 */
 	public static final int DISPENSER_FAILS = 1001;
 
 	/**
-	 * Dispenser launches a projectile
-	 * Plays the dispenser launch sound event
+	 * A Dispenser launches a projectile.
+	 * Plays the dispenser launch sound event.
 	 */
 	public static final int DISPENSER_LAUNCHES_PROJECTILE = 1002;
 
 	/**
-	 * An Eye of Ender is launched
-	 * Plays the eye of ender launching sound event
+	 * An Eye of Ender is launched.
+	 * Plays the eye of ender launching sound event.
 	 */
 	public static final int EYE_OF_ENDER_LAUNCHES = 1003;
 
 	/**
-	 * A Firework Rocket is shot
-	 * Plays the firework shoot sound event
+	 * A Firework Rocket is shot.
+	 * Plays the firework shoot sound event.
 	 */
 	public static final int FIREWORK_ROCKET_SHOOTS = 1004;
 
 	/**
-	 * An Iron Door is opened
-	 * Plays the iron door opening sound event
+	 * An Iron Door is opened.
+	 * Plays the iron door opening sound event.
 	 */
 	public static final int IRON_DOOR_OPENS = 1005;
 
 	/**
-	 * A Wooden Door is opened
-	 * Plays the wooden door opening sound event
+	 * A Wooden Door is opened.
+	 * Plays the wooden door opening sound event.
 	 */
 	public static final int WOODEN_DOOR_OPENS = 1006;
 
 	/**
-	 * A Wooden Trapdoor is opened
-	 * Plays the wooden trapdoor opening sound event
+	 * A Wooden Trapdoor is opened.
+	 * Plays the wooden trapdoor opening sound event.
 	 */
 	public static final int WOODEN_TRAPDOOR_OPENS = 1007;
 
 	/**
-	 * A Fence Gate is opened
-	 * Plays the fence gate opening sound event
+	 * A Fence Gate is opened.
+	 * Plays the fence gate opening sound event.
 	 */
 	public static final int FENCE_GATE_OPENS = 1008;
 
 	/**
-	 * A fire block or campfire is extinguished
-	 * Plays the fire extinguish sound event
+	 * A fire block or campfire is extinguished.
+	 * Plays the fire extinguish sound event.
 	 */
 	public static final int FIRE_EXTINGUISHED = 1009;
 
 	/**
-	 * A Music Disc is played
-	 * Plays the music disc's song
-	 * The Raw ID of the Music Disc item must be supplied as extra data
+	 * A Music Disc is played.
+	 * Plays the music disc's song.
+	 * The Raw ID of the Music Disc item must be supplied as extra data.
 	 */
 	public static final int MUSIC_DISC_PLAYED = 1010;
 
 	/**
-	 * An Iron Door is closed
-	 * Plays the iron door closing sound event
+	 * An Iron Door is closed.
+	 * Plays the iron door closing sound event.
 	 */
 	public static final int IRON_DOOR_CLOSES = 1011;
 
 	/**
-	 * A Wooden Door is closed
-	 * Plays the wooden door closing sound event
+	 * A Wooden Door is closed.
+	 * Plays the wooden door closing sound event.
 	 */
 	public static final int WOODEN_DOOR_CLOSES = 1012;
 
 	/**
-	 * A Wooden Trapdoor is closed
-	 * Plays the wooden trapdoor closing sound event
+	 * A Wooden Trapdoor is closed.
+	 * Plays the wooden trapdoor closing sound event.
 	 */
 	public static final int WOODEN_TRAPDOOR_CLOSES = 1013;
 
 	/**
-	 * A Fence Gate is closed
-	 * Plays the fence gate closing sound event
+	 * A Fence Gate is closed.
+	 * Plays the fence gate closing sound event.
 	 */
 	public static final int FENCE_GATE_CLOSES = 1014;
 
 	/**
-	 * A Ghast warns its victim
-	 * Plays the ghast warn sound event
+	 * A Ghast warns its victim.
+	 * Plays the ghast warn sound event.
 	 */
 	public static final int GHAST_WARNS = 1015;
 
 	/**
-	 * A Ghast shoots a fireball
-	 * Plays the ghast shoot sound event
+	 * A Ghast shoots a fireball.
+	 * Plays the ghast shoot sound event.
 	 */
 	public static final int GHAST_SHOOTS = 1016;
 
 	/**
-	 * An Ender Dragon shoots a fireball
-	 * Plays the ender dragon shoot sound event
+	 * An Ender Dragon shoots a fireball.
+	 * Plays the ender dragon shoot sound event.
 	 */
 	public static final int ENDER_DRAGON_SHOOTS = 1017;
 
 	/**
-	 * A Blaze shoots a fireball
-	 * Plays the blaze shoot sound event
+	 * A Blaze shoots a fireball.
+	 * Plays the blaze shoot sound event.
 	 */
 	public static final int BLAZE_SHOOTS = 1018;
 
 	/**
-	 * A Zombie attacks a Wooden Door
-	 * Plays the zombie attacking wooden door sound event
+	 * A Zombie attacks a Wooden Door.
+	 * Plays the zombie attacking wooden door sound event.
 	 */
 	public static final int ZOMBIE_ATTACKS_WOODEN_DOOR = 1019;
 
 	/**
-	 * A Zombie attacks an Iron Door
-	 * Plays the zombie attacking iron door sound event
+	 * A Zombie attacks an Iron Door.
+	 * Plays the zombie attacking iron door sound event.
 	 */
 	public static final int ZOMBIE_ATTACKS_IRON_DOOR = 1020;
 
 	/**
-	 * A Zombie breaks a Wooden Door
-	 * Plays the zombie breaking wooden door sound event
+	 * A Zombie breaks a Wooden Door.
+	 * Plays the zombie breaking wooden door sound event.
 	 */
 	public static final int ZOMBIE_BREAKS_WOODEN_DOOR = 1021;
 
 	/**
-	 * A Wither breaks a block
-	 * Plays the wither breaking block sound event
+	 * A Wither breaks a block.
+	 * Plays the wither breaking block sound event.
 	 */
 	public static final int WITHER_BREAKS_BLOCK = 1022;
 
 	/**
-	 * A Wither shoots a wither skull
-	 * Plays the wither shoot sound event
+	 * A Wither is spawned.
+	 * Plays the wither spawn sound event.
+	 * This is a global event.
+	 */
+	public static final int WITHER_SPAWNS = 1023;
+
+	/**
+	 * A Wither shoots a wither skull.
+	 * Plays the wither shoot sound event.
 	 */
 	public static final int WITHER_SHOOTS = 1024;
 
 	/**
-	 * A Bat takes off
-	 * Plays the bat take off sound event
+	 * A Bat takes off.
+	 * Plays the bat take off sound event.
 	 */
 	public static final int BAT_TAKES_OFF = 1025;
 
 	/**
-	 * A Zombie infects a Villager
-	 * Plays the zombie infect villager sound event
+	 * A Zombie infects a Villager.
+	 * Plays the zombie infect villager sound event.
 	 */
 	public static final int ZOMBIE_INFECTS_VILLAGER = 1026;
 
 	/**
-	 * A Zombie Villager is cured
-	 * Plays the zombie villager cured sound event
+	 * A Zombie Villager is cured.
+	 * Plays the zombie villager cured sound event.
 	 */
 	public static final int ZOMBIE_VILLAGER_CURED = 1027;
 
 	/**
-	 * An Anvil is destroyed from damage
-	 * Plays the anvil destroyed sound event
+	 * An Ender Dragon dies.
+	 * Plays the ender dragon death sound event.
+	 * This is a global event.
+	 */
+	public static final int ENDER_DRAGON_DIES = 1028;
+
+	/**
+	 * An Anvil is destroyed from damage.
+	 * Plays the anvil destroyed sound event.
 	 */
 	public static final int ANVIL_DESTROYED = 1029;
 
 	/**
-	 * An Anvil is used
-	 * Plays the anvil used sound event
+	 * An Anvil is used.
+	 * Plays the anvil used sound event.
 	 */
 	public static final int ANVIL_USED = 1030;
 
 	/**
-	 * An Anvil lands after falling
-	 * Plays the anvil landing sound event
+	 * An Anvil lands after falling.
+	 * Plays the anvil landing sound event.
 	 */
 	public static final int ANVIL_LANDS = 1031;
 
 	/**
-	 * A Portal is travelled through
-	 * Plays the portal travel sound event directly through the client's sound manager
+	 * A Portal is travelled through.
+	 * Plays the portal travel sound event directly through the client's sound manager.
 	 */
 	public static final int TRAVEL_THROUGH_PORTAL = 1032;
 
 	/**
-	 * A Chorus Flower grows
-	 * Plays the chorus flower growing sound event
+	 * A Chorus Flower grows.
+	 * Plays the chorus flower growing sound event.
 	 */
 	public static final int CHORUS_FLOWER_GROWS = 1033;
 
 	/**
-	 * A Chorus Flower dies
-	 * Plays the chorus flower death sound event
+	 * A Chorus Flower dies.
+	 * Plays the chorus flower death sound event.
 	 */
 	public static final int CHORUS_FLOWER_DIES = 1034;
 
 	/**
-	 * A Brewing Stand brews
-	 * Plays the brewing stand brewing sound event
+	 * A Brewing Stand brews.
+	 * Plays the brewing stand brewing sound event.
 	 */
 	public static final int BREWING_STAND_BREWS = 1035;
 
 	/**
-	 * An Iron Trapdoor closes
-	 * Plays the iron trapdoor closing sound event
+	 * An Iron Trapdoor closes.
+	 * Plays the iron trapdoor closing sound event.
 	 */
 	public static final int IRON_TRAPDOOR_CLOSES = 1036;
 
 	/**
-	 * An Iron Trapdoor opens
-	 * Plays the iron trapdoor opening sound event
+	 * An Iron Trapdoor opens.
+	 * Plays the iron trapdoor opening sound event.
 	 */
 	public static final int IRON_TRAPDOOR_OPENS = 1037;
 
 	/**
-	 * A Phantom bites its victim
-	 * Plays the phantom bite sound event
+	 * An End Portal is opened.
+	 * Plays the end portal spawn sound event.
+	 * This is a global event.
+	 */
+	public static final int END_PORTAL_OPENED = 1038;
+
+	/**
+	 * A Phantom bites its victim.
+	 * Plays the phantom bite sound event.
 	 */
 	public static final int PHANTOM_BITES = 1039;
 
 	/**
-	 * A Zombie converts into a Drowned
-	 * Plays the zombie convert to drowned sound event
+	 * A Zombie converts into a Drowned.
+	 * Plays the zombie convert to drowned sound event.
 	 */
 	public static final int ZOMBIE_CONVERTS_TO_DROWNED = 1040;
 
 	/**
-	 * A Husk converts into a Zombie
-	 * Plays the husk convert to zombie sound event
+	 * A Husk converts into a Zombie.
+	 * Plays the husk convert to zombie sound event.
 	 */
 	public static final int HUSK_CONVERTS_TO_ZOMBIE = 1041;
 
 	/**
-	 * A Grindstone is used
-	 * Plays the grindstone used sound event
+	 * A Grindstone is used.
+	 * Plays the grindstone used sound event.
 	 */
 	public static final int GRINDSTONE_USED = 1042;
 
 	/**
-	 * A page is turned in a Book on a Lectern
-	 * Plays the page turn sound event
+	 * A page is turned in a Book on a Lectern.
+	 * Plays the page turn sound event.
 	 */
 	public static final int LECTERN_BOOK_PAGE_TURNED = 1043;
 
 	/**
-	 * A Smithing Table is used
-	 * Plays the smithing table used sound event
+	 * A Smithing Table is used.
+	 * Plays the smithing table used sound event.
 	 */
 	public static final int SMITHING_TABLE_USED = 1044;
 
 	/**
-	 * Pointed Dripstone lands after falling
-	 * Plays the pointed dripstone landing sound event
+	 * Pointed Dripstone lands after falling.
+	 * Plays the pointed dripstone landing sound event.
 	 */
 	public static final int POINTED_DRIPSTONE_LANDS = 1045;
 
 	/**
-	 * Pointed Dripstone drips Lava into a Cauldron
-	 * Plays the pointed dripstone dripping lava into cauldron sound event
+	 * Pointed Dripstone drips Lava into a Cauldron.
+	 * Plays the pointed dripstone dripping lava into cauldron sound event.
 	 */
 	public static final int POINTED_DRIPSTONE_DRIPS_LAVA_INTO_CAULDRON = 1046;
 
 	/**
-	 * Pointed Dripstone drips Water into a Cauldron
-	 * Plays the pointed dripstone dripping water into cauldron sound event
+	 * Pointed Dripstone drips Water into a Cauldron.
+	 * Plays the pointed dripstone dripping water into cauldron sound event.
 	 */
 	public static final int POINTED_DRIPSTONE_DRIPS_WATER_INTO_CAULDRON = 1047;
 
 	/**
-	 * A Skeleton converts into a Stray
-	 * Plays the skeleton convert to stray sound event
+	 * A Skeleton converts into a Stray.
+	 * Plays the skeleton convert to stray sound event.
 	 */
 	public static final int SKELETON_CONVERTS_TO_STRAY = 1048;
 
 	/**
-	 * An item is composted in a Composter
-	 * Plays the appropriate composting sound event and spawns composter particles
-	 * A {@code 1} should be parsed as extra data if the use of the composter added to the level of compost inside
+	 * An item is composted in a Composter.
+	 * Plays the appropriate composting sound event and spawns composter particles.
+	 * A {@code 1} should be parsed as extra data if the use of the composter added to the level of compost inside.
 	 */
 	public static final int COMPOSTER_USED = 1500;
 
 	/**
-	 * Lava is extinguished
-	 * Plays the lava extinguish sound event and spawns large smoke particles
+	 * Lava is extinguished.
+	 * Plays the lava extinguish sound event and spawns large smoke particles.
 	 */
 	public static final int LAVA_EXTINGUISHED = 1501;
 
 	/**
-	 * A Redstone Torch burns out
-	 * Plays the redstone torch burn out sound event and spawns smoke particles
+	 * A Redstone Torch burns out.
+	 * Plays the redstone torch burn out sound event and spawns smoke particles.
 	 */
 	public static final int REDSTONE_TORCH_BURNS_OUT = 1502;
 
 	/**
-	 * An End Portal Frame is filled with an Eye of Ender
-	 * Plays the end portal frame filled sound event and spawns smoke particles
+	 * An End Portal Frame is filled with an Eye of Ender.
+	 * Plays the end portal frame filled sound event and spawns smoke particles.
 	 */
 	public static final int END_PORTAL_FRAME_FILLED = 1503;
 
 	/**
-	 * Pointed Dripstone drips fluid particles
-	 * Spawns dripping fluid particles
+	 * Pointed Dripstone drips fluid particles.
+	 * Spawns dripping fluid particles.
 	 */
 	public static final int POINTED_DRIPSTONE_DRIPS = 1504;
 
 	/**
-	 * A Dispenser is activated
-	 * Spawns smoke particles
-	 * The ordinal direction the dispenser is facing must be supplied as extra data
+	 * A Dispenser is activated.
+	 * Spawns smoke particles.
+	 * The ordinal direction the dispenser is facing must be supplied as extra data.
 	 */
 	public static final int DISPENSER_ACTIVATED = 2000;
 
 	/**
-	 * A block is broken
-	 * Plays the appropriate block breaking sound event and spawns block breaking particles
-	 * The raw ID of the block must be supplied as extra data
+	 * A block is broken.
+	 * Plays the appropriate block breaking sound event and spawns block breaking particles.
+	 * The raw ID of the block must be supplied as extra data.
 	 */
 	public static final int BLOCK_BROKEN = 2001;
 
 	/**
-	 * A non-instant Splash Potion is splashed
-	 * Plays the splash potion breaking sound event and spawns splash potion particles
-	 * The hex color of the potion must be supplied as extra data
-	 * For instant effects such as Instant Health and Instant Damage, use {@link INSTANT_SPLASH_POTION_SPLASHED}
+	 * A non-instant Splash Potion is splashed.
+	 * Plays the splash potion breaking sound event and spawns splash potion particles.
+	 * The hex color of the potion must be supplied as extra data.
+	 * For instant effects such as Instant Health and Instant Damage, use {@link INSTANT_SPLASH_POTION_SPLASHED}.
 	 */
 	public static final int SPLASH_POTION_SPLASHED = 2002;
 
 	/**
-	 * A thrown Eye of Ender breaks
-	 * Spawns several particles
+	 * A thrown Eye of Ender breaks.
+	 * Spawns several particles.
 	 */
 	public static final int EYE_OF_ENDER_BREAKS = 2003;
 
 	/**
-	 * A Spawner spawns mobs
-	 * Spawns smoke and flame particles
+	 * A Spawner spawns mobs.
+	 * Spawns smoke and flame particles.
 	 */
 	public static final int SPAWNER_SPAWNS = 2004;
 
 	/**
-	 * A plant is fertilized with Bone Meal or by a Bee
-	 * Spawns happy villager particles
+	 * A plant is fertilized with Bone Meal or by a Bee.
+	 * Spawns happy villager particles.
 	 */
-	public static final int CROPS_FERTILIZED = 2005;
+	public static final int PLANT_FERTILIZED = 2005;
 
 	/**
-	 * A Dragon Breath Cloud spawns
-	 * Plays the dragon fireball explode sound event and spawns dragon breath particles
+	 * A Dragon Breath Cloud spawns.
+	 * Plays the dragon fireball explode sound event and spawns dragon breath particles.
 	 */
 	public static final int DRAGON_BREATH_CLOUD_SPAWNS = 2006;
 
 	/**
-	 * An instant Splash Potion is splashed
-	 * Plays the splash potion breaking sound event and spawns instant splash potion particles
-	 * The hex color of the potion must be supplied as extra data
-	 * For non-instant effects, use {@link SPLASH_POTION_SPLASHED}
+	 * An instant Splash Potion is splashed.
+	 * Plays the splash potion breaking sound event and spawns instant splash potion particles.
+	 * The hex color of the potion must be supplied as extra data.
+	 * For non-instant effects, use {@link SPLASH_POTION_SPLASHED}.
 	 */
 	public static final int INSTANT_SPLASH_POTION_SPLASHED = 2007;
 
 	/**
-	 * An Ender Dragon breaks a block
-	 * Spawns an explosion particle
+	 * An Ender Dragon breaks a block.
+	 * Spawns an explosion particle.
 	 */
 	public static final int ENDER_DRAGON_BREAKS_BLOCK = 2008;
 
 	/**
-	 * A Wet Sponge dries out in a hot dimension
-	 * Spawns cloud particles
+	 * A Wet Sponge dries out in a hot dimension.
+	 * Spawns cloud particles.
 	 */
 	public static final int WET_SPONGE_DRIES_OUT = 2009;
 
 	/**
-	 * An End Gateway spawns
-	 * Plays the end gateway spawn sound event and spawns an explosion emitter particle
+	 * An End Gateway spawns.
+	 * Plays the end gateway spawn sound event and spawns an explosion emitter particle.
 	 */
 	public static final int END_GATEWAY_SPAWNS = 3000;
 
 	/**
-	 * The Ender Dragon is being resurrected
-	 * Plays the ender dragonm growl sound event
+	 * The Ender Dragon is being resurrected.
+	 * Plays the ender dragonm growl sound event.
 	 */
 	public static final int ENDER_DRAGON_RESURRECTED = 3001;
 
 	/**
-	 * Electricity sparks after Lightning hits a Lightning Rod
-	 * Spawns electric spark particles
+	 * Electricity sparks after Lightning hits a Lightning Rod.
+	 * Spawns electric spark particles.
 	 */
 	public static final int ELECTRICITY_SPARKS = 3002;
 
 	/**
-	 * A block is waxed
-	 * Plays the block waxing sound event and spawns waxing particles
+	 * A block is waxed.
+	 * Plays the block waxing sound event and spawns waxing particles.
 	 */
 	public static final int BLOCK_WAXED = 3003;
 
 	/**
-	 * Wax is removed from a block
-	 * Spawns wax removal particles
+	 * Wax is removed from a block.
+	 * Spawns wax removal particles.
 	 */
 	public static final int WAX_REMOVED = 3004;
 
 	/**
-	 * A block is scraped
-	 * Spawns scraping particles
+	 * A block is scraped.
+	 * Spawns scraping particles.
 	 */
 	public static final int BLOCK_SCRAPED = 3005;
 }
