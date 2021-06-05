597ec2bbfacc82be01d13c0be31f5c18ca4a4aa1
liach <liach@users.noreply.github.com>
2021-06-05 09:41:37 -0500

    Barebone javadoc for brain, explains why
    closes #2210, feel free to ask for more

---
+++ src/packageDocs/java/net/minecraft/entity/ai/goal/package-info.java
@@ -0,0 +1,13 @@
+/*
+ * This file is free for everyone to use under the Creative Commons Zero license.
+ */
+
+/**
+ * The basic AI system for Minecraft living entities.
+ *
+ * <p>This has been the dominant AI system in Minecraft since 1.8. In 1.14, with the update
+ * on villagers, a new {@linkplain net.minecraft.entity.ai.brain brain system} has been
+ * introduced.
+ */
+
+package net.minecraft.entity.ai.goal;
