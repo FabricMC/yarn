1591381578ea77f451b1f8f49b9e55761cdc9185
NebelNidas <nebelnidas@gmail.com>
2024-08-18 21:41:26 +0200

    Use custom visitor instead of an intermediate tree, check for more duplicate names

--- filament/src/main/java/net/fabricmc/filament/task/mappingio/CheckMappingsTask.java
+++ filament/src/main/java/net/fabricmc/filament/task/mappingio/CheckMappingsTask.java
@@ -1,5 +1,6 @@
 package net.fabricmc.filament.task.mappingio;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -7,56 +8,129 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import net.fabricmc.mappingio.MappedElementKind;
+import net.fabricmc.mappingio.MappingVisitor;
+
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.TaskAction;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.filament.task.base.FilamentTask;
-import net.fabricmc.filament.task.base.WithFileInput;
 import net.fabricmc.mappingio.MappingReader;
-import net.fabricmc.mappingio.tree.MappingTree;
-import net.fabricmc.mappingio.tree.MemoryMappingTree;
 
-public abstract class CheckMappingsTask extends FilamentTask implements WithFileInput {
+public abstract class CheckMappingsTask extends FilamentTask {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CheckMappingsTask.class);
+
+	@Input
+	abstract Property<File> getMappings();
 
 	@TaskAction
 	public final void run() throws IOException {
-		Path path = getInput().get().getAsFile().toPath();
-		MemoryMappingTree tree = new MemoryMappingTree();
-		MappingReader.read(path, tree);
+		Path path = getMappings().get().toPath();
+		List<String> errors = new ArrayList<>();
 
-		int minNamespaceId = tree.getMinNamespaceId();
-		int maxNamespaceId = tree.getMaxNamespaceId();
+		MappingReader.read(path, new MappingVisitor() {
+			private final Set<String> classes = new HashSet<>();
+			private final Set<String> members = new HashSet<>();
+			private final Set<Integer> argLvIndices = new HashSet<>();
+			private String clsSrcName;
+			private String memberSrcName;
+			private String memberId;
 
-		List<String> duplicates = new ArrayList<>();
+			@Override
+			public void visitNamespaces(String srcNamespace, List<String> dstNamespaces) throws IOException {
+			}
 
-		for (MappingTree.ClassMapping classMapping : tree.getClasses()) {
-			method: for (MappingTree.MethodMapping method : classMapping.getMethods()) {
-				Set<String> names = new HashSet<>();
-
-				for (int id = minNamespaceId; id < maxNamespaceId; id++) {
-					String name = method.getName(id);
-
-					if (name == null) {
-						break method;
-					}
-
-					names.add(name);
+			@Override
+			public boolean visitClass(String srcName) throws IOException {
+				if (classes.contains(srcName)) {
+					errors.add("Duplicate class mapping for " + srcName);
+				} else {
+					classes.add(srcName);
 				}
 
-				if (names.size() == 1 && method.getSrcName().startsWith("method_")) {
-					duplicates.add(classMapping.getSrcName() + "." + method.getSrcName());
+				members.clear();
+				clsSrcName = srcName;
+				return true;
+			}
+
+			@Override
+			public boolean visitField(String srcName, @Nullable String srcDesc) throws IOException {
+				memberId = srcName + ":" + srcDesc;
+
+				if (members.contains(memberId)) {
+					errors.add("Duplicate field mapping for " + clsSrcName + "#" + memberId);
+				} else {
+					members.add(memberId);
+				}
+
+				memberSrcName = srcName;
+				return true;
+			}
+
+			@Override
+			public boolean visitMethod(String srcName, @Nullable String srcDesc) throws IOException {
+				memberId = srcName + srcDesc;
+
+				if (members.contains(memberId)) {
+					errors.add("Duplicate method mapping for " + clsSrcName + "#" + memberId);
+				} else {
+					members.add(memberId);
+				}
+
+				argLvIndices.clear();
+				memberSrcName = srcName;
+				return true;
+			}
+
+			@Override
+			public boolean visitMethodArg(int argPosition, int lvIndex, @Nullable String srcName) throws IOException {
+				if (argLvIndices.contains(lvIndex)) {
+					errors.add("Duplicate parameter mapping for " + clsSrcName + "#" + memberId + ", slot " + lvIndex);
+				}
+
+				argLvIndices.add(lvIndex);
+				return true;
+			}
+
+			@Override
+			public boolean visitMethodVar(int lvtRowIndex, int lvIndex, int startOpIdx, int endOpIdx, @Nullable String srcName) throws IOException {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public void visitDstName(MappedElementKind targetKind, int namespace, String name) throws IOException {
+				switch (targetKind) {
+					case CLASS:
+						if (clsSrcName.equals(name)) {
+							errors.add("Same name in both namespaces for class " + clsSrcName);
+						}
+
+						break;
+					case FIELD:
+					case METHOD:
+						if (memberSrcName.equals(name) && (name.startsWith("method_") || name.startsWith("field_"))) {
+							errors.add("Same name in both namespaces for member " + clsSrcName + "#" + memberId);
+						}
+
+						break;
 				}
 			}
-		}
 
-		if (duplicates.isEmpty()) {
+			@Override
+			public void visitComment(MappedElementKind mappedElementKind, String comment) throws IOException {
+			}
+		});
+
+		if (errors.isEmpty()) {
 			return;
 		}
 
-		for (String duplicate : duplicates) {
-			LOGGER.error("Method {} has the same name in all namespaces", duplicate);
+		for (String error : errors) {
+			LOGGER.error(error);
 		}
 
 		throw new RuntimeException("Duplicate mappings detected");
